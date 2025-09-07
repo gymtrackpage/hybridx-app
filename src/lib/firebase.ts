ac0f8a0ff634a8875d8ac8b63fbf739879564206
Jon Lee <jdlee9900@gmail.com>
2025-09-07 17:34:04 +0000

    That's great, that's fixed the error. Thank you. I seem to not have pers

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, browserLocalPersistence, setPersistence } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,5 +18,12 @@ const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : get
 
 const db = getFirestore(app);
 const auth = getAuth(app);
+
+// Set persistence to local storage to keep the user signed in
+setPersistence(auth, browserLocalPersistence)
+  .catch((error) => {
+    console.error("Error setting auth persistence:", error);
+  });
+
 
 export { app, db, auth };
