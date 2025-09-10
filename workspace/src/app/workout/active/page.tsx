58e64210ab2c3b595389bdbe60e6706a3a6d2f0f
Jon Lee <jdlee9900@gmail.com>
2025-09-10 18:14:38 +0000

    Thanks. It's working better now. But we need to work through some layout

--- workspace/src/app/workout/active/page.tsx
+++ workspace/src/app/workout/active/page.tsx
@@ -214,13 +214,6 @@ export default function ActiveWorkoutPage() {
                         </div>
                     </div>
                 )}
-
-                <Button asChild variant="secondary" className="w-full">
-                    <Link href="https://timer.hybridx.club/" target="_blank">
-                        <Timer className="mr-2" />
-                        Use the Timer
-                    </Link>
-                </Button>
                 
                 <div>
                     <h3 className="text-base font-semibold mb-3">{isRunningProgram ? 'Runs:' : 'Exercises:'}</h3>
